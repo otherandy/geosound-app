e57fa629dad2346e7761ca5b7700bc7c2f1ec7fb
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-11-18 14:38:04 -0800

    changed env var

--- src/app/data/page.tsx
+++ src/app/data/page.tsx
@@ -2,13 +2,13 @@ import AudioDataMap from "@/components/data-map";
 import AudioDataTable from "@/components/data-table";
 
 export default async function AudioDataPage() {
-  const response = await fetch(process.env.API_URL + "/audio");
+  const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/audio");
   const audioData = await response.json();
 
   return (
     <main>
       <AudioDataMap data={audioData} />
-      <div className="container mx-auto pt-8 px-2">
+      <div className="container mx-auto py-8 px-2">
         <AudioDataTable data={audioData} />
       </div>
     </main>
