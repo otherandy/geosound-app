7f2037e73d77bf6d7fe2226fc92b8275f1168538
Andrés Herrera <18634968+otherandy@users.noreply.github.com>
2024-11-19 21:40:10 -0800

    changed globe to heatmap

--- src/components/data-globe.tsx
+++ src/components/data-globe.tsx
@@ -4,20 +4,25 @@ import Globe from "react-globe.gl";
 import type { AudioData } from "@/lib/types";
 
 export default function AudioDataGlobe({ data }: { data: AudioData[] }) {
-  const ringsData = data.map((audio) => ({
+  const gData = data.map((audio) => ({
     lat: audio.latitude,
     lng: audio.longitude,
-    maxR: audio.loudness,
+    weight: audio.loudness,
   }));
 
   return (
     <Globe
       globeImageUrl="/assets/3d/texture_earth.jpg"
       backgroundColor="white"
-      ringsData={ringsData}
-      ringMaxRadius={"maxR"}
-      width={600}
-      height={600}
+      width={550}
+      height={550}
+      heatmapsData={[gData]}
+      heatmapPointLat="lat"
+      heatmapPointLng="lng"
+      heatmapPointWeight="weight"
+      heatmapTopAltitude={0.5}
+      heatmapsTransitionDuration={3000}
+      pointerEventsFilter={(obj) => obj.type === "Feature"}
     />
   );
 }
